8f78f181c21a8d5c884f9dd12bd9b23f2f027bb4
elixirevo <elixirevocation@gmail.com>
2021-08-15 23:55:49 +0900

    Updates

---
+++ precache-manifest.a09ad00bbb4337b3d364eba08d506869.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "062c0ff5247a35790cc474d23b962025",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73c2e7a590cda2694877",
+    "url": "/static/css/main.14686620.chunk.css"
+  },
+  {
+    "revision": "0276af5e57da50a5d2c8",
+    "url": "/static/js/2.b0bbfce8.chunk.js"
+  },
+  {
+    "revision": "679dc0b0298984b03ab372d214526e9a",
+    "url": "/static/js/2.b0bbfce8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73c2e7a590cda2694877",
+    "url": "/static/js/main.dc450e91.chunk.js"
+  },
+  {
+    "revision": "ae2dfafbfc2365c4ffff",
+    "url": "/static/js/runtime-main.f7c5443e.js"
+  },
+  {
+    "revision": "8b63b6d0db62f70b6af4e5a930506837",
+    "url": "/static/media/popcatex-logo.8b63b6d0.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d835cd384ef5e82f99a08849b6a2adca.js"
+  "/precache-manifest.a09ad00bbb4337b3d364eba08d506869.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
